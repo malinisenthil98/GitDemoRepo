dc218a77d1a8748e45c1377606aea19f7a7419e1
Malini Senthilkumar <malinisenthil98@gmail.com>
2021-07-29 17:28:43 -0700

    Additional method ViewArray

--- ArrayPractice/src/MirrorArray.java
+++ ArrayPractice/src/MirrorArray.java
@@ -29,11 +29,19 @@ public class MirrorArray {
             }
         }
         System.out.println("Input Array ");
-        displayArray(a);
+        ViewArray(a);
         System.out.println("Mirror Array ");
         displayArray(b);
     }
     public static void displayArray(int array[][]){
+        for (int i = 0;i<array.length;i++){
+            for (int j =0;j<array[0].length;j++){
+                System.out.print(array[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void ViewArray(int array[][]){
         for (int i = 0;i<array.length;i++){
             for (int j =0;j<array[0].length;j++){
                 System.out.print(array[i][j]+"\t");
